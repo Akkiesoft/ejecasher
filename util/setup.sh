6347db8e214318eccc55f429df23212575d95c37
Akira Ouchi <akkiesoft@marokun.net>
2022-08-09 14:22:49 +0900

    [setup.sh] support different user name

--- util/setup.sh
+++ util/setup.sh
@@ -5,5 +5,5 @@ sudo apt install -y fonts-vlgothic
 
 mkdir -p ~/.config/lxsession/LXDE-pi
 cp autostart ~/.config/lxsession/LXDE-pi/autostart
-cp casher.desktop ~/.local/share/applications/casher.desktop
-sudo cp 99-toshibatec.rules /etc/udev/rules.d/99-toshibatec.rules
+sed -e "s/pi/$USER/g" casher.desktop | sudo tee ~/.local/share/applications/casher.desktop > /dev/null
+sed -e "s/pi/$USER/g" 99-toshibatec.rules | sudo tee /etc/udev/rules.d/99-toshibatec.rules > /dev/null
